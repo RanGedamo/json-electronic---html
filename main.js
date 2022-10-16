f6affdf6fb01af126148ca056729e860decece78
RanGedamo <mcrangedamo@gmail.com>
2022-10-16 08:01:40 +0300

    tv2069998

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 async function electronic(){
     try{
-        return await fetch("https://rangedamo.github.io/json-electronic---html/electronic.json").then(res=>res.json())
+        return await fetch("https://rangedamo.github.io/react-bulid-json/db.json").then(res=>res.json())
         .then(res=>console.log(res))
     }
     catch(err){console.log(err)}
