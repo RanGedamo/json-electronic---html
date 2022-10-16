adc1ee7d7e3c4daef7468d45a2f02ada51636ff4
RanGedamo <mcrangedamo@gmail.com>
2022-10-16 18:47:23 +0300

    fidge2

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-const PROJECTOR_API = "https://rangedamo.github.io/json-electronic---html/projector-apartments.json"
-const TV_API = "https://rangedamo.github.io/json-electronic---html/tv-apartments.json"
-const COUCH_API = "https://rangedamo.github.io/json-electronic---html/couch-apartments.json"
+const PROJECTOR_API = "https://rangedamo.github.io/json-electronic---html/projector-electronic.json"
+const TV_API = "https://rangedamo.github.io/json-electronic---html/tv-electronic.json"
+const COUCH_API = "https://rangedamo.github.io/json-electronic---html/couch-furniture.json"
+const FIDGE_API = "https://rangedamo.github.io/json-electronic---html/fridge-electronic.json"
 async function electronic(){
     try{
-        const result =  await fetch(COUCH_API).then(res=>res.json())
+        const result =  await fetch(FIDGE_API).then(res=>res.json())
         .then(res=>res)
         console.log(result)
         
         result.map(item=>{
             console.log(item.imageP)
-            projector.innerHTML += `<p>${item.nameCouch}</p><img src=${item.imageCouch} width='200px'> <br> <p>${item.dataCouch}</p>`
+            projector.innerHTML += `<p>${item.nameFridge}</p><img src=${item.imageCouch} width='200px'> <br> <p>${item.dataCouch}</p>`
         })
         
     }
