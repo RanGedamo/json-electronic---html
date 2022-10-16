a99d65b2720eca01d18d091eac4a1967ca6d58f3
RanGedamo <mcrangedamo@gmail.com>
2022-10-16 17:07:07 +0300

    projector

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ async function electronic(){
         const result =  await fetch("https://rangedamo.github.io/json-electronic---html/tv-apartments.json").then(res=>res.json())
         .then(res=>res)
         console.log(result)
+        
         result.map(item=>{
-            console.log(item.nametv)
+            const imageTvs = document.createElement('div')
+            console.log(item.imageTv)
+            imageTvs.innerHTML = `<img src=${item.imageTv} width='200px'>`
         })
         
     }
