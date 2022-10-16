1296cac4eec41acf5553a72c555bf2086920beb7
RanGedamo <mcrangedamo@gmail.com>
2022-10-16 18:42:16 +0300

    fidge

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const PROJECTOR_API = "https://rangedamo.github.io/json-electronic---html/projector-apartments.json"
 const TV_API = "https://rangedamo.github.io/json-electronic---html/tv-apartments.json"
+const COUCH_API = "https://rangedamo.github.io/json-electronic---html/couch-apartments.json"
 async function electronic(){
     try{
-        const result =  await fetch(PROJECTOR_API).then(res=>res.json())
+        const result =  await fetch(COUCH_API).then(res=>res.json())
         .then(res=>res)
         console.log(result)
         
         result.map(item=>{
             console.log(item.imageP)
-            projector.innerHTML += `<p>${item.nameProjector}</p><img src=${item.imageProjector} width='200px'> <br> <p>${item.dataProjector}</p>`
+            projector.innerHTML += `<p>${item.nameCouch}</p><img src=${item.imageCouch} width='200px'> <br> <p>${item.dataCouch}</p>`
         })
         
     }
